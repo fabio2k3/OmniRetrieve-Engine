86cf11c60e1bc25fae65ab827dfd34680f5516d3
GlenRios <glendariosrodriguez3@gmail.com>
2026-05-23 20:58:08 -0400

    fix(database): added functions to __init__

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -3,6 +3,7 @@ from .crawler_repository import (
     upsert_document, save_pdf_text, save_pdf_error,
     get_pending_pdf_ids, get_document, document_exists,
     log_crawl_start, log_crawl_end, get_stats,
+    get_document_counts, get_unindexed_pdf_count,
 )
 from .chunk_repository import (
     save_chunks, get_chunks,
@@ -11,7 +12,8 @@ from .chunk_repository import (
     get_all_embeddings_iter,
     get_chunk_count, get_embedded_count, get_chunk_stats,
     get_chunks_by_ids,
-    reset_embeddings,
+    reset_embeddings, get_chunks_with_metadata_by_arxiv_ids,
+    get_chunk_embeddings_by_ids, 
 )
 from .embedding_repository import (
     init_embedding_schema,
@@ -25,6 +27,7 @@ from .index_repository import (
     get_unindexed_documents, mark_documents_indexed, get_index_stats,
     get_top_terms, get_postings_for_term,
     get_postings_for_matrix, get_document_metadata,
+    get_indexed_doc_count, get_term_words_by_ids,
 )
 
 __all__ = [
@@ -33,7 +36,8 @@ __all__ = [
     # crawler_repository
     "upsert_document", "save_pdf_text", "save_pdf_error",
     "get_pending_pdf_ids", "get_document", "document_exists",
-    "log_crawl_start", "log_crawl_end", "get_stats",
+    "log_crawl_start", "log_crawl_end", "get_stats", "get_document_counts",
+    "get_unindexed_pdf_count",
     # chunk_repository
     "save_chunks", "get_chunks",
     "save_chunk_embedding", "save_chunk_embeddings_batch",
@@ -41,7 +45,7 @@ __all__ = [
     "get_all_embeddings_iter",
     "get_chunk_count", "get_embedded_count", "get_chunk_stats",
     "get_chunks_by_ids",
-    "reset_embeddings",
+    "reset_embeddings", "get_chunks_with_metadata_by_arxiv_ids",
     # embedding_repository
     "init_embedding_schema", "log_faiss_build",
     "save_embedding_meta", "get_embedding_meta", "get_embedding_stats",
@@ -49,5 +53,6 @@ __all__ = [
     "clear_index", "upsert_terms", "flush_postings", "save_index_meta",
     "get_unindexed_documents", "mark_documents_indexed", "get_index_stats",
     "get_top_terms", "get_postings_for_term",
-    "get_postings_for_matrix", "get_document_metadata",
+    "get_postings_for_matrix", "get_document_metadata", ""
+    "get_indexed_doc_count", "get_term_words_by_ids",
 ]
